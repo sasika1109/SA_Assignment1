bcff713ec1822489a5e5c2de0f1c5e112b79c5e3
sasika9511 <sashi951109@gmail.com>
2020-04-03 23:58:12 +0530

    online food ordering with bill payment

---
+++ Test osgi/ServicePublisher/src/servicepublisher/Activator.java
@@ -0,0 +1,35 @@
+package servicepublisher;
+
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator implements BundleActivator {
+
+	ServiceRegistration publishServiceRegistration;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Publisher Start");
+		 Hashtable<String, String> props = new Hashtable<String, String>();
+	        props.put("Language", "English");
+		ServicePublish publisherService = new ServicePublishImpl();
+		publishServiceRegistration = context.registerService(
+				ServicePublish.class.getName(), publisherService, props);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("Publisher Stop");
+		publishServiceRegistration.unregister();
+	}
+
+}
