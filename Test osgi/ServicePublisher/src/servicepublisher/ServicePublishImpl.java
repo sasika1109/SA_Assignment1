3dd2b9372e4b84f548232e93b227e2258453c133
khan9920 <shafkhanmohamed23@gmail.com>
2020-04-04 19:54:13 +0530

    Code refactored

--- Test osgi/ServicePublisher/src/servicepublisher/ServicePublishImpl.java
+++ Test osgi/ServicePublisher/src/servicepublisher/ServicePublishImpl.java
@@ -2,7 +2,7 @@ package servicepublisher;
 
 public class ServicePublishImpl implements ServicePublish{
 
-	String[] Stock = { "koththu", "rice", "parata", "hoppers", "stream hoppers" };
+	String[] Stock = { "koththu", "rice", "parata", "hoppers", "string hoppers" };
 	 
 	@Override
 	public String publishService() {
@@ -10,74 +10,54 @@ public class ServicePublishImpl implements ServicePublish{
 	}
 
 	
-	public boolean checkAvailability(String foodName) 
-	{
+	public boolean checkAvailability(String foodName) {
 		
 		foodName = foodName.toLowerCase();
-
-	            // This is very inefficient
-	            for (int i = 0; i < Stock.length; i++)
-	            {
-	                if (Stock[i].equals(foodName))
-	                {
+			// This is very inefficient
+			for (int i = 0; i < Stock.length; i++) {
+	                if (Stock[i].equals(foodName)) {
 	                    return true;
 	                }
 	            }
 		return false;
 	}
-
-
 	
 	public int  checkReply(int reply , String food) {
 		
-		if(reply == 1 && food.equals("koththu")) {
+		if (reply == 1 && food.equals("koththu")) {
 			int price = 250;
 			return price;
-		}
-		else if(reply == 1 && food.equals("rice")) {
+		} else if(reply == 1 && food.equals("rice")) {
 			int price = 180;
 			return price;
-		}
-		else if(reply == 1 && food.equals("parata")) {
+		} else if(reply == 1 && food.equals("parata")) {
 			int price = 60;
 			return price;
-		}
-		else {
+		} else {
 			return 0;
 		}
-		
 	}
 	
 	public boolean checkReply1(int reply) {
-		if(reply == 1)
-		{
+		if(reply == 1) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 	
-	public int generateBill(int count , String food)
-	{
-		if(food.equals("koththu"))
-		{
+	public int generateBill(int count , String food) {
+		
+		if(food.equals("koththu")) {
 			int total = 250 * count;
 			return total;
-		}
-		else if(food.equals("rice"))
-		{
+		} else if(food.equals("rice")) {
 			int total = 180 * count;
 			return total;
-		}
-		
-		else if(food.equals("parata"))
-		{
+		} else if(food.equals("parata")) {
 			int total = 60 * count;
 			return total;
-		}
-		else {
+		} else {
 			return 0;
 		}
 	}
